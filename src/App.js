6d95a6a82201c2c6acd918fa527f5fa489e3abc1
Luke Ghenco <lukeghenco@gmail.com>
2017-05-31 18:57:11 -0400

    Update App.js to successfully receive weatherData state

--- src/App.js
+++ src/App.js
@@ -9,24 +9,42 @@ class App extends Component {
   constructor() {
     super() 
 
+    this.state = {
+      fetchingWeatherData: true,
+      weatherData: {},
+      forecastKey: null,
+    }
   }
 
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(position => {
       return fetchJsonp(`${APIURL}${position.coords.latitude},${position.coords.longitude}`)
       .then(response => response.json())
-      .then(data => console.log(data))
+      .then(data => this.setState({ 
+        fetchingWeatherData: false,
+        weatherData: data,
+        forecastKey: 'currently'
+      }))
     }) 
   }
 
   render() {
-    console.log(this.state)
+    const { weatherData, fetchingWeatherData, forecastKey } = this.state
+    const forecast = weatherData[forecastKey]
+    console.log(forecast)
     return (
       <div>
         <h1>Weather App</h1>
+        {fetchingWeatherData ?
+          <p>Loading...</p>
+          :
+          <div>
+            Forecast and Navbar go here
+          </div>
+        }
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
