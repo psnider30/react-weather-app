ed65e3e5ba994f3b9f5675d07050a777e39fb8b1
Luke Ghenco <lukeghenco@gmail.com>
2017-05-31 14:22:51 -0400

    Set up fetch request for Dark Sky Api Fetch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import fetchJsonp from 'fetch-jsonp'
+
+const APIURL = `https://api.darksky.net/forecast/${process.env.REACT_APP_DARK_SKY}/`
 
 class App extends Component {
+
+  constructor() {
+    super() 
+
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(position => {
+      return fetchJsonp(`${APIURL}${position.coords.latitude},${position.coords.longitude}`)
+      .then(response => response.json())
+      .then(data => console.log(data))
+    }) 
+  }
+
   render() {
+    console.log(this.state)
     return (
       <div>
         <h1>Weather App</h1>
