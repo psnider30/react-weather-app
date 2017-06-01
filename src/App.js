399623a0eb5aa0a0b21bc184cac470f365a972be
Luke Ghenco <lukeghenco@gmail.com>
2017-05-31 20:14:25 -0400

    Create initial App.js state

--- src/App.js
+++ src/App.js
@@ -3,15 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super() 
+
+    this.state = {
+      fetchingData: true,
+      weatherData: {}
+    }
+  }
+  
   render() {
+    const { fetchingData } = this.state
+    console.log(fetchingData)
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Weather App</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {
+            fetchingData ?
+            <img src={logo} className="App-logo" alt="logo" />
+            :
+            <h1>Data is received</h1>
+          }
         </p>
       </div>
     );
